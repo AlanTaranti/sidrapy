99977238ef5433bd83ff6b01267f81280984ea1a
Alan Taranti <alan.taranti@gmail.com>
2020-06-20 15:34:27 -0400

    removido dependencias requests[security]

--- setup.py
+++ setup.py
@@ -1,14 +1,5 @@
-# -*- coding: utf-8 -*-
 from setuptools import setup
 from src.sidrapy import VERSION
 
 # Metadata goes in setup.cfg. These are here for GitHub's dependency graph.
-setup(
-    name="sidrapy",
-    version=VERSION,
-    install_requires=[
-        'requests',
-        'requests[security]',
-        'pandas',
-    ]
-)
+setup(name="sidrapy", version=VERSION, install_requires=["requests", "pandas"])
