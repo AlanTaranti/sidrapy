4483509a37a23c33a9d94aebbba11996d7cb87dd
Alan Taranti <alan.taranti@gmail.com>
2020-06-25 19:13:29 -0400

    centralizado a versao em arquivo

--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
 from setuptools import setup
 
-VERSION = "0.1.1"
+with open("__version__") as version_file:
+    VERSION = version_file.read().strip()
 
 # Metadata goes in setup.cfg. These are here for GitHub's dependency graph.
 setup(name="sidrapy", version=VERSION, install_requires=["requests", "pandas"])
