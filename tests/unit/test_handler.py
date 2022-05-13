30a803a6a2de9d537ce8de427a42c8d6cea985ae
Alan Taranti <contato@alan.taranti.me>
2022-05-12 22:52:16 -0400

    ajustado testes

--- tests/unit/test_handler.py
+++ tests/unit/test_handler.py
@@ -1,4 +1,4 @@
-import random
+from random import randint, random
 from unittest.mock import Mock
 from unittest.mock import patch
 from uuid import uuid4
@@ -7,7 +7,7 @@ import pytest
 from src import sidrapy
 
 
-def random_args():
+def random_args_classification():
     args = [
         "table_code",
         "territorial_level",
@@ -20,9 +20,52 @@ def random_args():
     ]
     kwargs = {k: str(uuid4()) for k in args}
     for arg in args[3:]:
-        if random.random() < 0.5:
+        if random() < 0.5:
             del kwargs[arg]
+
+    if "classification" not in kwargs and "categories" in kwargs:
+        del kwargs["categories"]
+
     return kwargs
+
+
+def get_random_classifications() -> dict[str, str]:
+    classifications = {}
+    for _ in range(randint(0, 10)):
+        categories = [str(uuid4()) for __ in range(randint(0, 10))]
+        classifications[str(uuid4())] = ",".join(categories)
+
+    return classifications
+
+
+def random_args():
+    args = [
+        "table_code",
+        "territorial_level",
+        "ibge_territorial_code",
+        "variable",
+        "classifications",
+        "period",
+        "header",
+    ]
+    kwargs = {k: str(uuid4()) for k in args}
+    kwargs["classifications"] = get_random_classifications()
+
+    for arg in args[3:]:
+        if random() < 0.5:
+            del kwargs[arg]
+
+    return kwargs
+
+
+def test_get_url_classification():
+    for _ in range(5_000):
+        kwargs = random_args_classification()
+        url = sidrapy.resources.handler.get_url(**kwargs)
+        assert isinstance(url, str)
+        assert url.startswith("https://apisidra.ibge.gov.br/values/")
+        for value in kwargs.values():
+            assert value in url
 
 
 def test_get_url():
@@ -32,14 +75,19 @@ def test_get_url():
         assert isinstance(url, str)
         assert url.startswith("https://apisidra.ibge.gov.br/values/")
         for value in kwargs.values():
-            assert value in url
+            if type(value) == "str":
+                assert value in url
+            elif type(value) == "dict":
+                for k, v in value.items():
+                    assert k in url
+                    assert v in url
 
 
 def test_get_ok():
     mock_response = Mock()
     mock_response.ok = True
     with patch("src.sidrapy.resources.handler.requests") as mock_request:
-        kwargs = random_args()
+        kwargs = random_args_classification()
         mock_request.get.return_value = mock_response
         response = sidrapy.resources.handler.get(**kwargs)
         assert response is mock_response.json()
@@ -49,7 +97,7 @@ def test_get_not_ok():
     mock_response = Mock()
     mock_response.ok = False
     with patch("src.sidrapy.resources.handler.requests") as mock_request:
-        kwargs = random_args()
+        kwargs = random_args_classification()
         mock_request.get.return_value = mock_response
         with pytest.raises(ValueError):
             sidrapy.resources.handler.get(**kwargs)
